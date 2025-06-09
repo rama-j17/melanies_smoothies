265b3fc978a5643b042dd1db0a559b2f4da7b3b3
rama-j17 <j.rama3617@gmail.com>
2025-06-10 00:46:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, m
 
 if ingredients_list:
     # Build the ingredient string
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+    import json
+    ingredients_json = json.dumps(ingredients_list)
+
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
